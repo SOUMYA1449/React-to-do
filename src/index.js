b9e3058f9709cca22de212bb523bbf27e4306fbb
Soumya Ranjan Mishra <152312390+SOUMYA1449@users.noreply.github.com>
2024-11-23 22:13:48 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import store from './redux/store'; // Import the store from redux store.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap the App component with the Redux Provider */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
